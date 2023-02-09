8e61a4a7cf581ba17385050d7bc1f439e2874ea0
kovlanproj <kozorizva@gmail.com>
2023-02-09 14:29:18 +0200

    some changes

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,9 +7,9 @@ const {SECRET_KEY} = process.env;
 const auth = async (req, res, next) => {
  const {authorization = ""} = req.headers;
  const [bearer, token] = authorization.split(" ");
- 
+
  try {
-    if (bearer !== "Bearer") {
+    if ((bearer !== "Bearer") && (token !=='')) {
         throw new Unauthorized("Not authorized");
      }
     const {id} = jwt.verify(token, SECRET_KEY);
